1cf4076f8cd0ba4d99d16a0aa011b5c6bb5608a5
Clay O'Dell <clay.odell.music@gmail.com>
2024-12-15 16:54:17 -0600

    config updated

--- config.js
+++ config.js
@@ -13,20 +13,13 @@ const PORT = +process.env.PORT || 3001;
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
       ? "jobly_test"
-      : process.env.SUPABASE_URI;
+      : process.env.DATABASE_URI;
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
 //
 // WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
-
-console.log("Jobly Config:".green);
-console.log("SECRET_KEY:".yellow, SECRET_KEY);
-console.log("PORT:".yellow, PORT.toString());
-console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
-console.log("Database:".yellow, getDatabaseUri());
-console.log("---");
 
 module.exports = {
   SECRET_KEY,
